727c13604a1678a7ffad85f07769d6470308f140
Duncan.Patel <Duncan.Patel@fdmgroup.com>
2024-07-12 08:43:42 -0700

    Created/implemented/tested FeeCalculationService

--- src/main/java/com/fdmgroup/service/FeeCalculationServiceImpl.java
+++ src/main/java/com/fdmgroup/service/FeeCalculationServiceImpl.java
@@ -2,20 +2,29 @@ package com.fdmgroup.service;
 
 public class FeeCalculationServiceImpl implements FeeCalculationService {
 	
+	/*---------------------------------------------------------------------------------------------------------------*/
+	/*Attributes*/
+	/*---------------------------------------------------------------------------------------------------------------*/
 	private double accountBalanceFee;
 
-	@Override
+	/*---------------------------------------------------------------------------------------------------------------*/
+	/*Implemented Methods*/
+	/*---------------------------------------------------------------------------------------------------------------*/
 	public double calculateFee(double balance) {
-		if(balance <= 100)
-			accountBalanceFee = 20.00;
-		else if(balance > 100 || balance <= 500)
-			accountBalanceFee = 15.00;
-		else if(balance > 500 || balance <= 1_000)
-			accountBalanceFee = 10.00;
-		else if(balance > 1_000 || balance <= 2_000)
-			accountBalanceFee = 5.00;
-		else if(balance > 2_000)
-			accountBalanceFee = 0.00;
+		if(balance > 2_000)
+			accountBalanceFee = 0d;
+		
+		if(balance > 1_000 && balance <= 2_000)
+			accountBalanceFee = 5d;
+		
+		else if(balance > 500 && balance <= 1_000)
+			accountBalanceFee = 10d;
+		
+		else if(balance > 100 && balance <= 500)
+			accountBalanceFee = 15d;
+		
+		else if(balance <= 100)
+			accountBalanceFee = 20d;
 		
 		return accountBalanceFee;
 	}
--- src/test/java/com/fdmgroup/service/FeeCalculationServiceTest.java
+++ src/test/java/com/fdmgroup/service/FeeCalculationServiceTest.java
@@ -1,38 +1,105 @@
 package com.fdmgroup.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.fdmgroup.model.Account;
+import com.fdmgroup.model.CheckingAccount;
+
 public class FeeCalculationServiceTest {
 	
+	/*---------------------------------------------------------------------------------------------------------------*/
+	/*Attributes*/
+	/*---------------------------------------------------------------------------------------------------------------*/
+	double testBalance;
+	private Account testAccount;
 	private FeeCalculationServiceImpl feeCalculationServiceImpl;
 	
-//	@Test
-//	void test() {
-//		// Arrange
-//		
-//		// Act
-//
-//		// Assert
-//
-//	}
-	
+	/*---------------------------------------------------------------------------------------------------------------*/
+	/*Set Up Methods*/ 
+	/*---------------------------------------------------------------------------------------------------------------*/
 	@BeforeEach
 	void setUp() throws Exception {
+		testAccount = new CheckingAccount();
 		feeCalculationServiceImpl = new FeeCalculationServiceImpl();
 	}
 	
+	/*---------------------------------------------------------------------------------------------------------------*/
+	/*Test Methods*/ 
+	/*---------------------------------------------------------------------------------------------------------------*/
 	@Test
-	void testWhenBalance_LessThanOrEqual$100_Return$20Fee() {
+	void testWhenBalance_lessThanOrEqual$100_return$20Fee() {
 		// Arrange
-		double expectedResult = 20.00;
-		
-		// Stubbing
+		testBalance = 100d;
+		testAccount.correctBalance(testBalance);
+		double expectedResult = 20d;
 		
 		// Act
-		double actualResult = feeCalculationServiceImpl.calculateFee(0);
+		double actualResult = feeCalculationServiceImpl.calculateFee(testAccount.getBalance());
+		
 		// Assert
-
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	void testWhenBalance_greaterThan$100_orLessThanOrEqual$500_return$15Fee() {
+		// Arrange
+		testBalance = 500d;
+		testAccount.correctBalance(testBalance);
+		double expectedResult = 15d;
+		
+		// Act
+		double actualResult = feeCalculationServiceImpl.calculateFee(testAccount.getBalance());
+		
+		// Assert
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	void testWhenBalance_greaterThan$500_orLessThanOrEqual$1k_return$10Fee() {
+		// Arrange
+		testBalance = 1_000d;
+		testAccount.correctBalance(testBalance);
+		double expectedResult = 10d;
+		
+		// Act
+		double actualResult = feeCalculationServiceImpl.calculateFee(testAccount.getBalance());
+		
+		// Assert
+		assertEquals(expectedResult, actualResult);
+		//assertEquals(testAccount.getBalance(), testBalance);
+	}
+	
+	@Test
+	void testWhenBalance_greaterThan$1k_orLessThanOrEqual$2k_return$5Fee() {
+		// Arrange
+		testBalance = 2_000d;
+		testAccount.correctBalance(testBalance);
+		
+		double expectedResult = 5d;
+		
+		// Act
+		double actualResult = feeCalculationServiceImpl.calculateFee(testAccount.getBalance());
+		
+		// Assert
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	void testWhenBalance_greaterThan$2k_return$0Fee() {
+		// Arrange
+		testBalance = 2_001d;
+		testAccount.correctBalance(testBalance);
+		
+		double expectedResult = 0d;
+		
+		// Act
+		double actualResult = feeCalculationServiceImpl.calculateFee(testAccount.getBalance());
+		
+		// Assert
+		assertEquals(expectedResult, actualResult);
 	}
 	
 }
