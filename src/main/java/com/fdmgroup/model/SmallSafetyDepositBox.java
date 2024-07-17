c72a89c28edb304d2e9b87a56c251fd4ca8c18e4
Duncan.Patel <Duncan.Patel@fdmgroup.com>
2024-07-17 12:26:29 -0700

    Created scaffolding for Story 3

---
+++ src/main/java/com/fdmgroup/model/SmallSafetyDepositBox.java
@@ -0,0 +1,14 @@
+package com.fdmgroup.model;
+
+public class SmallSafetyDepositBox extends SafetyDepositBox {
+	// max limit of safety deposit boxes
+	private double capacity;
+
+	public double getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(double capacity) {
+		this.capacity = capacity;
+	}
+}
