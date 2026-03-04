078d630c4b04a9d4c540cbd03bda08cc4a9032ff
Duncan.Patel <Duncan.Patel@fdmgroup.com>
2026-03-04 09:13:34 -0800

    Added ChargeAllAccounts() method and method logic.

--- AccountAndCustomerManagement/src/main/java/model/Company.java
+++ AccountAndCustomerManagement/src/main/java/model/Company.java
@@ -1,7 +1,21 @@
 package model;
 
+import java.util.List;
+
 public class Company extends Customer {
 	public Company(String name, String address) {
 		super(name, address);
+	}
+
+	@Override
+	public void chargeAllAccounts(double amount) {
+		List<Account> allAccounts = getAccounts();
+		for (Account a : allAccounts) {
+			if(a instanceof CheckingAccount)
+				a.withdraw(amount);
+			else if(a instanceof SavingsAccount)
+				a.withdraw(2 * amount);
+		}
+		
 	}
 }
--- AccountAndCustomerManagement/src/main/java/model/Person.java
+++ AccountAndCustomerManagement/src/main/java/model/Person.java
@@ -1,7 +1,17 @@
 package model;
 
+import java.util.List;
+
 public class Person extends Customer {
 	public Person(String name, String address) {
 		super(name, address);
+	}
+	
+	@Override
+	public void chargeAllAccounts(double amount) {
+		List<Account> allAccounts = getAccounts();
+		for (Account a : allAccounts) {
+			a.withdraw(amount);
+		}	
 	}
 }
