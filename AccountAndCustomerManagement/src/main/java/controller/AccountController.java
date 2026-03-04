0f5698d4423da6a4b215543db069d7e624a56ed2
Duncan.Patel <Duncan.Patel@fdmgroup.com>
2026-03-04 12:57:18 -0800

    Finished all methods.

--- AccountAndCustomerManagement/src/main/java/controller/AccountController.java
+++ AccountAndCustomerManagement/src/main/java/controller/AccountController.java
@@ -1,44 +1,63 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import model.Account;
-import model.Company;
-import model.Customer;
-import model.Person;
+import model.account.Account;
+import model.account.CheckingAccount;
+import model.account.SavingsAccount;
+import model.customer.Company;
+import model.customer.Customer;
+import model.customer.Person;
 
 public class AccountController {
-	private List<Customer> customers;
-	private List<Account> accounts;
+	private List<Customer> customers = new ArrayList<>();
+    private List<Account> accounts = new ArrayList<>();
 	
 	public Customer createCustomer(String name, String address, String type) {
-		if(type.toLowerCase() == "person") {
+		if(type.trim().equalsIgnoreCase("person")) {
 			Person person = new Person(name, address);
 			customers.add(person);
 			return person;
 		}
-		else if(type.toLowerCase().trim() == "company"){
+		else if(type.trim().equalsIgnoreCase("company")) {
 			Company company  = new Company(name, address);
 			customers.add(company);
 			return company;
 		}
-		else
-			return null;
+
+		return null;
 	}
 	
 	public Account createAccount(Customer customer, String type) {
-		return null;
+		if(type.trim().equalsIgnoreCase("checking")) {
+			CheckingAccount checking = new CheckingAccount();
+			accounts.add(checking);
+			customer.addAccount(checking);
+			return checking;
+		}
+		else if(type.trim().equalsIgnoreCase("savings")) {
+			SavingsAccount savings = new SavingsAccount();
+			accounts.add(savings);
+			customer.addAccount(savings);
+			return savings;
+		}
 		
+		return null;
 	}
 	
 	public void removeCustomer(Customer customer) {
 		customers.remove(customer);
-		
+		for (Account account : customer.getAccounts()) {
+				accounts.remove(account);
+		}
 	}
 	
 	public void removeAccount(Account account) {
 		accounts.remove(account);
-		
+		for(Customer customer : customers) {
+			customer.removeAccount(account);
+		}
 	}
 
 	public List<Customer> getCustomers() {
