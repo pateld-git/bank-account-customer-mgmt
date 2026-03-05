18f01753d9c094132718e5aa6a6929464b862920
Duncan.Patel <Duncan.Patel@fdmgroup.com>
2026-03-05 11:54:18 -0800

    Added constructor

--- AccountAndCustomerManagement/src/main/java/controller/AccountController.java
+++ AccountAndCustomerManagement/src/main/java/controller/AccountController.java
@@ -11,8 +11,14 @@ import model.customer.Customer;
 import model.customer.Person;
 
 public class AccountController {
-	private List<Customer> customers = new ArrayList<>();
-    private List<Account> accounts = new ArrayList<>();
+	private List<Customer> customers;
+    private List<Account> accounts;
+    
+    public AccountController() {
+    	super();;
+    	customers = new ArrayList<>();
+        accounts = new ArrayList<>();
+	}
 	
 	public Customer createCustomer(String name, String address, String type) {
 		if(type.trim().equalsIgnoreCase("person")) {
