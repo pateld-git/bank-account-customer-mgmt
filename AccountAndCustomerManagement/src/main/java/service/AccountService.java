faf8fb5a50d6c9dc5f300e5143e00ae6830a9689
Duncan.Patel <Duncan.Patel@fdmgroup.com>
2026-03-18 11:10:23 -0700

    Added methods and implementation methods

--- AccountAndCustomerManagement/src/main/java/service/AccountService.java
+++ AccountAndCustomerManagement/src/main/java/service/AccountService.java
@@ -1,5 +1,13 @@
 package service;
 
+import java.util.List;
+
+import dto.account.Account;
+
 public interface AccountService {
+
+	public List<Account> getAllAccounts();
+	void removeAccount(Account deleteAccount);
+	void createAccount(Account newAccount);
 
 }
