f1b448f00962111def5b23664f6526a1abde674d
Duncan.Patel <Duncan.Patel@fdmgroup.com>
2026-03-18 11:30:54 -0700

    Changed balance tier names

--- AccountAndCustomerManagement/src/main/java/model/BankBalanceTier.java
+++ AccountAndCustomerManagement/src/main/java/model/BankBalanceTier.java
@@ -1,8 +1,8 @@
 package model;
 
 public enum BankBalanceTier {
-	LOW_BALANCE(100.00, 20.00), MID_BALANCE(500.00, 15.00), HIGH_BALANCE(1000.00, 10.00),
-	PREMIUM_BALANCE(2000.00, 5.00);
+	BOTTOM_BALANCE(100.00, 20.00), LOW_BALANCE(500.00, 15.00), MEDIUM_BALANCE(1000.00, 10.00),
+	HIGH_BALANCE(2000.00, 5.00);
 
 	private final double maxTierBalance;
 	private final double fee;
