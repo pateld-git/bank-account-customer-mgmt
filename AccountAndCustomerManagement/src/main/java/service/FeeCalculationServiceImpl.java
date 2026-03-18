78af9ef189a6fb09c7ced9456d41a8c682348931
Duncan.Patel <Duncan.Patel@fdmgroup.com>
2026-03-18 11:21:00 -0700

    Added debug and warn logging

--- AccountAndCustomerManagement/src/main/java/service/FeeCalculationServiceImpl.java
+++ AccountAndCustomerManagement/src/main/java/service/FeeCalculationServiceImpl.java
@@ -1,16 +1,22 @@
 package service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import model.BankBalanceTier;
 
 public class FeeCalculationServiceImpl implements FeeCalculationService {
+	private static final Logger LOGGER = LogManager.getLogger(FeeCalculationServiceImpl.class);
 
 	public double calculateFee(double balance) {
 		for (BankBalanceTier tier : BankBalanceTier.values()) {
 			if (balance <= tier.getMaxTierBalance()) {
+				LOGGER.debug("Balance matches Tier: {}", tier);
 				return tier.getFee();
 			}
 		}
 
+		LOGGER.warn("No matching Tier for balance ${} defined in BankBalanceTier. Fee set to $0", balance);
 		return 0;
 	}
 
