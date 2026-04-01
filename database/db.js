a7e46ad72690af77258f7f9cc9e86a2275b3f2d4
Duncan.Patel <Duncan.Patel@fdmgroup.com>
2026-04-01 12:17:27 -0700

    Error checking for mongodb uri

--- database/db.js
+++ database/db.js
@@ -2,13 +2,14 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        if (!process.env.MONGODB_URI) {
+            throw new Error("MONGODB_URI is not defined in .env file");
+        }
+        console.log('Connecting to:', process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('MongoDB connected');
     } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
+        console.error('Error connecting to MongoDB:\n', error);
         process.exit(1);
     }
 };
