5aadd8164302909459a0be0eb66d89be83180498
Duncan.Patel <Duncan.Patel@fdmgroup.com>
2026-04-01 12:13:38 -0700

    var name change

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ import transactionRoutes from './routes/transaction-routes.js';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-connectDB();
+await connectDB();
 
 app.use(express.json());
 
 app.use('/accounts', accountRoutes);
 app.use('/transactions', transactionRoutes);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
