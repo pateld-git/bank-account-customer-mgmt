b4b39af7567720efc96e81f9ca84958ac553eba6
Duncan.Patel <Duncan.Patel@fdmgroup.com>
2026-04-04 11:19:17 -0700

    added routing for user auth

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import connectDB from './database/db.js';
 import accountRoutes from './routes/account-routes.js';
 import transactionRoutes from './routes/transaction-routes.js';
+import authRoutes from './routes/auth-routes.js';
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.use(express.json());
 
 app.use('/accounts', accountRoutes);
 app.use('/accounts', transactionRoutes);
+app.use('/auth', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
