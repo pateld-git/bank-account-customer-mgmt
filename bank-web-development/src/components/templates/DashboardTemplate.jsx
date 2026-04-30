c6f9e50c5382f96be4650888cd80f25fdd33a939
Duncan.Patel <Duncan.Patel@fdmgroup.com>
2026-04-30 06:07:14 -0700

    Dashboard Template

---
+++ bank-web-development/src/components/templates/DashboardTemplate.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import Header from "../organisms/Header/Header";
+import SideMenu from "../organisms/SideMenu/SideMenu";
+import { ROUTES } from "../../constants/Routes";
+import "./DashboardTemplate.css";
+
+/**
+ * DashboardTemplate component.
+ * Provides a consistent layout with a Header, SideNavBar, and a main content area.
+ */
+const DashboardTemplate = ({ children }) => {
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 768);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+      setIsDesktop(window.innerWidth > 768);
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const navLinks = Object.values(ROUTES)
+    .filter((route) => route.label)
+    .map((route) => ({
+      to: route.path,
+      label: route.label,
+    }));
+
+  const homeLink = [
+    { to: ROUTES.HOME.path, label: "Bank Account Application" },
+  ];
+
+  return (
+    <div className="dashboard-layout">
+      {isMobile && <SideMenu links={navLinks} />}
+      <Header links={homeLink} className="main-header" />
+      {isDesktop && <Header links={navLinks} className="sub-header" />}
+
+      <div className="dashboard-body">
+        <div className="main-content-area">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardTemplate;
