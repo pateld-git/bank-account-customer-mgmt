3acd161a19aff17e5e1ba21713f246bbd2dab5bc
Duncan.Patel <Duncan.Patel@fdmgroup.com>
2026-04-30 06:08:43 -0700

    React routing

--- bank-web-development/src/App.js
+++ bank-web-development/src/App.js
@@ -1,13 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { ROUTES } from "./constants/Routes";
 import './App.css';
-import Home from './pages/Home.jsx';
+import Update from './components/pages/Update/Update';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/home" element={<Home />}></Route>
+        {/* Map through the object values to generate routes */}
+        {Object.values(ROUTES).map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+        <Route path="/home" element={ROUTES.HOME.element} />
+        <Route path="/update/:type/:id" element={<Update />} />
+        <Route path="*" element={<Navigate to={ROUTES.HOME.path} replace />} />
       </Routes>
     </div >
   );
---
+++ bank-web-development/src/constants/Routes.js
@@ -0,0 +1,31 @@
+import Home from "../components/pages/Home/Home";
+import AddPage from "../components/pages/Add/Add";
+import UpdatePage from "../components/pages/Update/Update";
+import FindByIdPage from "../components/pages/FindById/FindByIdPage";
+import FindByBalancePage from "../components/pages/FindByBalance/FindByBalancePage";
+import CustomersPage from "../components/pages/Customers/CustomerPage";
+
+
+/**
+ * Configuration object for application routing.
+ * Defines the path, navigation label, and component element for each route.
+ * 
+ * @typedef {Object} ROUTES
+ * @property {string} path - The URL path for the route.
+ * @property {string} label - The display name for navigation links.
+ * @property {JSX.Element} element - The React component to render for this route.
+ */
+
+/**
+ * The collection of all available routes in the application.
+ * @type {Object.<string, RouteConfig>}
+ */
+
+export const ROUTES = {
+    HOME: { path: "/", label: "Home", element: <Home /> },
+    ADD: { path: "/add", label: "Add", element: <AddPage /> },
+    UPDATE: { path: "/updated", label: "Update", element: <UpdatePage /> },
+    CUSTOMERS: { path: "/customers", label: "Customers", element: <CustomersPage /> },
+    FIND_BY_ID: { path: "/find-by-id", label: "Find by ID", element: <FindByIdPage /> },
+    FIND_BY_BALANCE: { path: "/find-by-balance", label: "Find by Balance", element: <FindByBalancePage /> },
+};
