4c58727287b26ac349289a17dde9b5482e3a916e
Duncan.Patel <Duncan.Patel@fdmgroup.com>
2026-04-30 09:22:35 -0700

    side menu active by default

--- bank-web-development/src/components/templates/DashboardTemplate.jsx
+++ bank-web-development/src/components/templates/DashboardTemplate.jsx
@@ -10,11 +10,9 @@ import "./DashboardTemplate.css";
  */
 const DashboardTemplate = ({ children }) => {
   const [isDesktop, setIsDesktop] = useState(window.innerWidth > 768);
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
       setIsDesktop(window.innerWidth > 768);
     };
     window.addEventListener("resize", handleResize);
@@ -35,7 +33,7 @@ const DashboardTemplate = ({ children }) => {
 
   return (
     <div className="dashboard-layout">
-      {isMobile && <SideMenu links={navLinks} />}
+      <SideMenu links={navLinks} />
       <Header links={homeLink} className="main-header" />
       {isDesktop && <Header links={navLinks} className="sub-header" />}
 
